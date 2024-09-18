0510be6370749efea1a7fe1db20a7dca4ac13751
llucgalce04 <llucgalce@gmail.com>
2024-09-18 11:59:26 +0200

    Ejercicio 4

--- primer.py
+++ primer.py
@@ -1,3 +1,4 @@
+from math import *
 #Ejercicio 1
 a = 3
 b = 4
@@ -22,6 +23,14 @@ height = 7.26
 
 perimeter = (width*2) + (height*2)
 area = (width * height)
-diagonal = (width**2) + (height**2)**0.5
+diagonal = ((width**2) + (height**2))**0.5
 
-print("The perimeter of the rectangle is:", perimeter, "and the area is:", area, "and finally the diagonal of teh rectangle is:", diagonal)
+print("The perimeter of the rectangle is:", perimeter, "and the area is:", area, "and finally the diagonal of the rectangle is:", diagonal)
+
+#Ejercicio 4
+
+numberMonths = 60
+anualInterest = (4.25/100)/12
+finalValue = 1000
+initalDeposit = (finalValue)/((1 + anualInterest)**numberMonths)
+print("The inital deposit is ", initalDeposit)
