43fac000d47e21eb92caa7dab13d3a643caa0e75
llucgalce04 <llucgalce@gmail.com>
2024-09-18 11:29:04 +0200

    Ejercicio 3

--- primer.py
+++ primer.py
@@ -14,3 +14,14 @@ time = 0.5
 
 accelerationResult = (finalVelocity-inicialVelocity)/time
 print("The avarage acceleration is ", accelerationResult)
+
+#Ejercicio 3
+
+width = 4.25
+height = 7.26
+
+perimeter = (width*2) + (height*2)
+area = (width * height)
+diagonal = (width**2) + (height**2)**1/2
+
+print("The perimeter of the rectangle is:", perimeter, "and the area is:", area, "and finally the diagonal of teh rectangle is:", diagonal)
